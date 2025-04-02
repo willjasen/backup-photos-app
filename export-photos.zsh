5232854cc35a2b754b488a0cd238c91ae6dda720
willjasen <brandon@willjasen.com>
2025-04-02 17:55:26 -0400

    export by person

--- export-photos.zsh
+++ export-photos.zsh
@@ -20,32 +20,35 @@ FROM_DATE='2025-03-01';
 TO_DATE='2025-04-30';
 REPORTS_DIR_NAME="-reports-";
 CHECKPOINTS=100;
-PHOTOS_ALBUMS=("Food");
+PHOTOS_ALBUMS=();
+PEOPLE=();
 
 # Define a function wrapping osxphotos export with default parameters to export an album
 export_album() {
-  local album="$1"
-  echo "Processing album: $album"  # Added echo for album
-  mkdir -p "${PHOTO_BACKUP_DIR}/${album}/${REPORTS_DIR_NAME}"   # Ensure reports directory exists
-  osxphotos export \
-    --library ${PHOTOS_LIBRARY_DIR} \
-    --download-missing \
-    --use-photokit \
-    --update \
-    --ramdb \
-    --checkpoint $CHECKPOINTS \
-    --export-by-date \
-    --report "${PHOTO_BACKUP_DIR}/${album}/${REPORTS_DIR_NAME}/export-${TIMESTAMP}.sqlite" \
-    \
-    --album "${album}" \
-    "${PHOTO_BACKUP_DIR}/${album}" \
-    ;
+    TIMESTAMP=$(date "+%Y%m%d%H%M%S")
+    local album="$1"
+    echo "\033[0;32mProcessing album: $album\033[0m"  # Changed echo to green output
+    mkdir -p "${PHOTO_BACKUP_DIR}/${album}/${REPORTS_DIR_NAME}"   # Ensure reports directory exists
+    osxphotos export \
+        --library ${PHOTOS_LIBRARY_DIR} \
+        --download-missing \
+        --use-photokit \
+        --update \
+        --ramdb \
+        --checkpoint $CHECKPOINTS \
+        --export-by-date \
+        --report "${PHOTO_BACKUP_DIR}/${album}/${REPORTS_DIR_NAME}/export-${TIMESTAMP}.sqlite" \
+        \
+        --album "${album}" \
+        "${PHOTO_BACKUP_DIR}/${album}" \
+        ;
 }
 
 # Define a function wrapping osxphotos export with default parameters to all photos/videos between two dates
 export_by_date() {
+    TIMESTAMP=$(date "+%Y%m%d%H%M%S")
     local by_date_dir_name="-by-date-";
-    echo "Processing all photos between $FROM_DATE and $TO_DATE";
+    echo "\033[0;32mProcessing all photos between $FROM_DATE and $TO_DATE\033[0m";  # Changed echo to green output
     mkdir -p "${PHOTO_BACKUP_DIR}/${by_date_dir_name}/${REPORTS_DIR_NAME}";
     osxphotos export \
     --library ${PHOTOS_LIBRARY_DIR} \
@@ -63,16 +66,40 @@ export_by_date() {
     ;
 }
 
+export_by_person() {
+    TIMESTAMP=$(date "+%Y%m%d%H%M%S")
+    local person=$1
+    local by_person_dir_name="-by-person-";
+    echo "\033[0;32mProcessing all photos by person $1\033[0m";  # Changed echo to green output
+    mkdir -p "${PHOTO_BACKUP_DIR}/${by_person_dir_name}/${person}/${REPORTS_DIR_NAME}";
+    osxphotos export \
+    --library ${PHOTOS_LIBRARY_DIR} \
+    --download-missing \
+    --use-photokit \
+    --update \
+    --ramdb \
+    --checkpoint $CHECKPOINTS \
+    --export-by-date \
+    --report "${PHOTO_BACKUP_DIR}/${by_person_dir_name}/${person}/${REPORTS_DIR_NAME}/export-${TIMESTAMP}.sqlite" \
+    \
+    --person $person \
+    "${PHOTO_BACKUP_DIR}/${by_person_dir_name}/${person}" \
+    ;
+}
+
 # Cycle through each album to backup
 for album in "${PHOTOS_ALBUMS[@]}"; do
     TIMESTAMP=$(date "+%Y%m%d%H%M%S")
-    mkdir -p "${PHOTO_BACKUP_DIR}/${album}"
     export_album $album
 done
 
+# Export all photos/videos by person
+for person in "${PEOPLE[@]}"; do
+    export_by_person $person
+done
+
 # Export all photos between dates
-TIMESTAMP=$(date "+%Y%m%d%H%M%S")
-export_by_date
+# export_by_date
 
 
-## --post-command exported "echo {shell_quote,{filepath}{comma}{,+keyword,}} >> {shell_quote,{export_dir}/exported.txt}" 
+## --post-command exported "echo {shell_quote,{filepath}{comma}{,+keyword,}} >> {shell_quote,{export_dir}/exported.txt}"
