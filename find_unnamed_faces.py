0ec0c1b856d1690d18b0fb2263b36b73a0bbd6b1
willjasen <brandon@willjasen.com>
2025-04-10 12:17:29 -0400

    fix comments

--- find_unnamed_faces.py
+++ find_unnamed_faces.py
@@ -1,16 +1,11 @@
-""" query function to be used with osxphotos query --query-function to find photos with identified face circles that are unnamed.
-
-See: https://www.reddit.com/r/osxphotos/comments/16o3wbs/finding_unnamed_faces_more_than_apple_photos_shows/
-
-Run with `osxphotos query --query-function find_unnamed_faces.py::unnamed_faces --add-to-album "Unnamed Faces" --quiet`
-"""
+# query function to be used with osxphotos query --query-function to find photos with identified face circles that are unnamed.
+# See: https://www.reddit.com/r/osxphotos/comments/16o3wbs/finding_unnamed_faces_more_than_apple_photos_shows/
+# Run with `osxphotos query --query-function find_unnamed_faces.py::unnamed_faces --add-to-album "Unnamed Faces" --quiet`
 
 from __future__ import annotations
-
 from osxphotos import PhotoInfo
 
 
-# call this with --query-function examples/query_function.py::best_selfies
 def unnamed_faces(photos: list[PhotoInfo]) -> list[PhotoInfo]:
     """your query function should take a list of PhotoInfo objects and return a list of PhotoInfo objects (or empty list)"""
 
